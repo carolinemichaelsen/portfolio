97c103a43d724db22ea02aff3acc17254f37257f
Caroline Michaelsen <carolinemichaelsen@gmail.com>
2023-09-25 18:26:40 -0300

    atualização de conteúdo e ajustes de layout em Projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,20 +11,22 @@ const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] })
 export default function Projects() { return(
 <div className="m-12 pt-10 sm:pt-18 lg:pt-24">
   <div>
-    <h3 className='text-zinc-900 font-bold text-4xl text-center mb-8 font-serif'>Projetos</h3>
+    <h3 className='text-zinc-900 font-bold text-4xl text-center mb-8 font-h3'>Projetos</h3>
   </div>
-  <div className='flex flex-row columns-3 p-20 items-stretch gap-8'>
+  <div className='flex flex-row columns-3  p-20 items-stretch gap-10'>
+
     {/* PROJETO 1 */}
-    <div className="p-5 bg-zinc-400 opacity-90 rounded-lg outline hover:outline-zinc-300">
-      <p className="text-zinc-900 font-bold text-s p-2 font-h3">Clone do Spotify Web</p>
-      <Image src='/imgs/spotify.png' width={200} height={100} alt='' className='hover:opacity-80 cursor-pointer rounded-md items-center m-2'/>
-      <div className='flex flex-col p-4'>
-        <p className='text-xs text-justify max-w-xs mb-2'>Reprodução da interface do Spotify Web, construída com <b>React</b>, <b>TypeScript</b> e <b>Tailwind.</b></p>
+    <div className="p-8 bg-zinc-400 opacity-90 rounded-lg outline hover:outline-zinc-300">
+      <p className="text-zinc-900 font-bold text-s font-h3 mb-6">Clone do Spotify Web</p>
+      <Image src='/imgs/spotify.png' width={200} height={100} alt='' className='hover:opacity-80 cursor-pointer rounded-md items-center'/>
+      <div className='flex flex-col'>
+        <p className='text-xs text-justify max-w-xs mt-6 mb-2'>Reprodução da interface do Spotify Web, construída com <b>React</b>, <b>TypeScript</b> e <b>Tailwind</b>.</p>
         <div className='flex flex-row gap-2 mt-2'>
           <BiLogoReact />
           <BiLogoTypescript />
           <BiLogoTailwindCss />
         </div>
+        <a href='https://github.com/carolinemichaelsen/spotify-clone' target='_blank'><button className="flex flex-row gap-2 bg-zinc-300 hover:bg-purple-200 text-zinc-700 font-semibold hover:text-purple300 py-2 px-2 w-24 border border-purple-500 hover:border-transparent rounded text-xs mb-4 mt-8 font-h3"><AiFillGithub />Detalhes</button></a>
         <div>
 
         </div>
@@ -33,18 +35,41 @@ export default function Projects() { return(
     </div>
 
     {/* PROJETO 2 */}
-    <div className="p-5 bg-zinc-400 opacity-90 rounded-lg outline hover:outline-zinc-300">
-      <p className=" space text-zinc-900 font-bold text-s font-h3">Site responsivo</p>
-      <Image src='/imgs/preview-mobile.png' width={200} height={100} alt='' className='hover:opacity-80 cursor-pointer rounded-md items-center m-2'/>
-      <p className='text-xs text-clip max-w-xs mb-2'>Plataforma pra encontrar trabalho remoto. Usei <b>React</b> e <b>Tailwind</b> criar uma página responsiva e com um menu com navegação dropdown.</p>
-      <button className="flex flex-row gap-2 bg-zinc-300 hover:bg-purple-400 text-zinc-700 font-semibold hover:text-white py-2 px-6 border border-purple-500 hover:border-transparent rounded text-xs mb-10"><AiFillGithub />Detalhes</button>
+    <div className="p-8 bg-zinc-400 opacity-90 rounded-lg outline hover:outline-zinc-300">
+      <p className="text-zinc-900 font-bold text-s font-h3 mb-6">Site Responsivo</p>
+      <Image src='/imgs/preview-mobile.png' width={200} height={100} alt='' className='hover:opacity-80 cursor-pointer rounded-md items-center'/>
+      <div className='flex flex-col'>
+        <p className='text-xs text-justify max-w-xs mt-6 mb-2'>Usei <b>React</b> e <b>Tailwind</b> criar uma página home responsiva e com um menu com navegação dropdown.</p>
+        <div className='flex flex-row gap-2 mt-2'>
+          <BiLogoReact />
+          <BiLogoTypescript />
+          <BiLogoTailwindCss />
+        </div>
+        <a href='https://github.com/carolinemichaelsen/intro-section-dropdown' target='_blank'><button className="flex flex-row gap-2 bg-zinc-300 hover:bg-purple-200 text-zinc-700 font-semibold hover:text-purple300 py-2 px-2 w-24 border border-purple-500 hover:border-transparent rounded text-xs mb-4 mt-8 font-h3"><AiFillGithub />Detalhes</button></a>
+        <div>
+
+        </div>
+      </div>
+
     </div>
 
     {/* PROJETO 3 */}
-    <div className="p-5 bg-zinc-400 opacity-90 rounded-lg outline hover:outline-zinc-300">
-      <p className="text-zinc-900 font-bold text-s p-2 font-h3">Clone do Spotify Web</p>
-      <Image src='/imgs/spotify.png' width={200} height={100} alt='' className='hover:opacity-80 cursor-pointer rounded-md items-center m-2'/>
-      <p className='text-xs text-justify max-w-xs mb-2'>Clone da interface do Spotify Web, construído com Tailwind.</p>
+    <div className="flex flex-col p-14 bg-zinc-400 opacity-90 rounded-lg outline hover:outline-zinc-300">
+      <p className="text-zinc-900 font-bold text-s font-h3 mb-6">HTML & CSS</p>
+      <Image src='/imgs/time-tracking.png' width={200} height={100} alt='' className='hover:opacity-80 cursor-pointer rounded-md items-center'/>
+      <div className='flex flex-col'>
+        <p className='text-xs text-justify max-w-xs mt-6 mb-2'>Solução para o desafio do Frontend Mentor.</p>
+        <div className='flex flex-row gap-2 mt-2'>
+          <BiLogoReact />
+          <BiLogoTypescript />
+          <BiLogoTailwindCss />
+        </div>
+        <a href='https://github.com/carolinemichaelsen/time-tracking' target='_blank'><button className="flex flex-row gap-2 bg-zinc-300 hover:bg-purple-200 text-zinc-700 font-semibold hover:text-purple300 py-2 px-2 w-24 border border-purple-500 hover:border-transparent rounded text-xs mb-4 mt-8 font-h3"><AiFillGithub />Detalhes</button></a>
+        <div>
+
+        </div>
+      </div>
+
     </div>
   </div>
 </div>
