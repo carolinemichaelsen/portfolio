66559864db9cfb4e4db9c3c9fca0a3e4857d0f31
Caroline Michaelsen <carolinemichaelsen@gmail.com>
2023-10-04 15:05:38 -0300

    ajustes de conteúdo e tipografia em About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,28 +1,29 @@
 import React from "react"
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+import '../app/globals.css'
 
 export default function About() {
 
     return (
-        <div className='flex flex-row bg-gray-300 shadow-lg shadow-gray-500 justify-center w-full columns-2'>
-            <div className='text-xl leading-loose text-center'>
+        <div className='flex flex-row bg-gray-300 shadow-lg shadow-gray-500 justify-center w-full columns-2 font-poppins'>
+            <div className='text-xl leading-loose'>
                 <p className='p-20 text-sm max-w-md text-left'>
-                Migrando da comunicação para o desenvolvimento front-end. 
+                Migrando da comunicação para o desenvolvimento front-end.
                 <br></br>
                 <br></br>
                 Há pouco mais de <RoughNotation type='circle' show={true} color='blue'>um ano</RoughNotation> estudando em plataformas como Alura, Coursera, Scrimba e freeCodeCamp.
                 </p>
             </div>
-            <div className='p-20 leading-loose text-left'>
+            <div className='p-20 leading-loose text-right'>
                 <p className=' text-sm max-w-md'>
                 Formação <RoughNotation type='circle' show={true} color='red'>React</RoughNotation> na Alura.</p>
-                <p className="text-xs mt-2">Tempo total: <RoughNotation type="highlight" show={true} color="#ffd54f">80 horas.</RoughNotation></p>
+                <p className="text-xs mt-2">Tempo total: <RoughNotation type="highlight" show={true} color="#ffd54f">67 horas.</RoughNotation></p>
                 <p className='p-2 text-sm max-w-md'>
                 Formação <RoughNotation type='underline' show={true} color='purple'>JavaScript</RoughNotation> na Alura.</p>
-                <p className="text-xs mt-2">Tempo total: <RoughNotation type="highlight" show={true} color="#ffd54f">80 horas.</RoughNotation></p>
+                <p className="text-xs mt-2">Tempo total: <RoughNotation type="highlight" show={true} color="#ffd54f">65 horas.</RoughNotation></p>
                 <p className='p-2 text-sm max-w-md'>
                 Formação <RoughNotation type='underline' show={true} color='purple'>React Native</RoughNotation> na Alura.<br></br>
-                Em andamento.
+                <p className="text-xs mt-2">Em andamento.</p> 
                 </p>    
             </div>
         </div>
